4ee5b3f30066870c007c5bedb8bf2cb242b1ecb0
IsabelGouveia <isabelgouveia@hotmail.com>
2023-11-26 13:56:12 +0000

    adding bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 
 const App= () => {
-  const [hello, setHello] = useState('')
+  const [hello, setHello] = useState('');
+  const [input, setInput] = useState('Portugal')
 
   useEffect (() => {
     // Fetch data from my backend
-    fetch(process.env.REACT_APP_BACKEND_API)
+    fetch(process.env.REACT_APP_BACKEND_API + input)
       .then((response) => response.text())
       .then((data) => {
         setHello(data);
