bedf2e7a6a9a96ebd12678c9d2da67026eb5a504
IsabelGouveia <isabelgouveia@hotmail.com>
2023-11-28 17:42:24 +0000

    adding new properties, adjusting maps currencies, timezones and languages

--- frontend/src/component/countryComponent.js
+++ frontend/src/component/countryComponent.js
@@ -1,10 +1,32 @@
 import '../styles/countryComponent.css'
 
 export const CountryComponent = ({ country }) => {
+    
+    // Mapping currencies
+  const currencies = Object.values(country.currencies).map((currency, index) => (
+    <span key={index}>{currency.name} ({currency.symbol})</span>
+  ));
+
+  // Mapping languages
+  const languages = Object.entries(country.languages).map(([code, name], index) => (
+    <span key={index}>{name} ({code}), </span>
+  ));
+  
+   // Mapping timezones
+  const timezones = country.timezones.map((timezone, index) => (
+    <span key={index}>{timezone}, </span>
+  )); 
+    
     return (
         <div className="countryDetails">
             <h1>{country.commonName}</h1>
             <p>Region: {country.region}</p>
+            <p>Currencies: {currencies}</p>
+            <p>Capital: {country.capital}</p>
+            <p>Languages: {languages}</p>
+            <p>Population: {country.population}</p>
+            <p>Timezone: {timezones}</p>
+            <p>Flags: {country.flagsPng}</p>
         </div>
     )
 
